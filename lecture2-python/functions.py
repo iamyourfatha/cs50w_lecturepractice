aa06a425c66513bb70dbf3e0770699891c8d9437
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-04 16:31:06 -0400

    .

--- lecture2-python/functions.py
+++ lecture2-python/functions.py
@@ -1,6 +1,8 @@
 def square(x): 
     return x * x
 
-for i in range(10):
-    print(f"The square of {i} is {square(i)}")
+#defining them in different files
+#this file links to squares.py
+#for i in range(10):
+ #   print(f"The square of {i} is {square(i)}")
     
---
+++ lecture2-python/squares.py
@@ -0,0 +1,6 @@
+#this file connects, references, to functions.py
+from functions import square
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
+    
