ded241c5524b8c8db657999ec264bc39f27bed3f
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-04 16:00:52 -0400

    ...

---
+++ lecture2-python/lists.py
@@ -0,0 +1,11 @@
+# comment: Define a List of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+print(names)
+print(names[3])
+
+names.append("Draco")
+print(names)
+
+names.sort()
+print(names)
--- lecture2-python/sequences.py
+++ lecture2-python/sequences.py
@@ -1,9 +1,13 @@
 name = "Harry"
-
 print(name)
 print(name[0])
 print(name[3])
 
-
 names = ["Hermione", "Harry", "Ron"]
 print(names)
+print(names[0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
