8d88898fdf989c44a8db8a40bd35c25950887b83
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-04 16:33:36 -0400

    squares function alternative call method

---
+++ lecture2-python/squares2.py
@@ -0,0 +1,9 @@
+import functions
+#if you do not specify what you ase importinh
+# from functions import squares
+
+for i in range(10):
+    print(f"The square of {i} is {functions.square(i)}")
+    #then you have to specify it in the function you need it for
+    # functions.square : the file.function that your importing
+
