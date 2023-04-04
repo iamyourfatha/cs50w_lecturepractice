1a839fae1908e46f6820320fbd8dfc187effdca8
iamyourfatha <75233097+iamyourfatha@users.noreply.github.com>
2023-04-04 16:12:11 -0400

    Update loops.py

---
+++ lecture2-python/loops.py
@@ -0,0 +1,14 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+    
+for j in range(6):
+    print(j)
+
+names = ["Harry", "Ron", "Hermione"]
+for name in names:
+    print(name)
+    
+name = "Harry"
+for character in name:
+    print(character)
+    
